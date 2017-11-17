716235aa4a1880daded12a8a0d2e6006f39f00d8
Paul Kocialkowski <contact@paulk.fr>
2017-11-17 14:02:39 +0100

    Add buttons controller code to hello example
    Signed-off-by: Paul Kocialkowski <contact@paulk.fr>

--- C/hello/Sources/main.c
+++ C/hello/Sources/main.c
@@ -6,5 +6,16 @@
 
 
 int main(int argc, char ** argv) {
+	unsigned long buttons;
 	puts("Hello world !\n");
+
+	while (1) {
+		if (MemoryRead(0x20000110) == 0)
+			continue;
+
+		buttons = MemoryRead(0x20000100);
+		puts("button status: ");
+		print_hex(buttons);
+		puts("\n");
+	}
 }
