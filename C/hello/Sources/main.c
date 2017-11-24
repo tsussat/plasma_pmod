1ffc54c5d0d457877198067f38bc56fb6603a934
Camille Leroux <camille.leroux@ims-bordeaux.fr>
2017-11-24 15:10:48 +0100

    modif the hello program so that it uses all the peripherals: buttons+VGA so far

--- C/hello/Sources/main.c
+++ C/hello/Sources/main.c
@@ -7,15 +7,27 @@
 
 int main(int argc, char ** argv) {
 	unsigned long buttons;
+
+	int H = 480; // H of the VGA screen in pixel
+	int W = 640; // W of the VGA screen in pixel
+
 	puts("Hello world !\n");
 
 	while (1) {
-		//if (MemoryRead(0x20000110) == 0)
-		//	continue;
 
 		buttons = MemoryRead(0x20000100);
 		puts("button status: ");
 		print_hex(buttons);
 		puts("\n");
+
+		for(int py = 0; py < H; py++)
+		{
+			for(int px = 0; px < W; px ++)
+			{
+				int i = px+py;
+				int pixel = ((i>>4)<<8|(i>>4)<<4|(i>>4));
+				MemoryWrite(0x20000120, pixel);		
+			}
+		}
 	}
 }
