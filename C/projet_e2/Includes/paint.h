51c6b20ad45f0fbdde719763048162ca93947b64
SUSSAT Theo <tsussat@singapour.pedago.ipb.fr>
2018-03-19 14:49:44 +0100

    paint.h + debut main

---
+++ C/projet_e2/Includes/paint.h
@@ -0,0 +1,18 @@
+//Function used to color a pixel at a given position (row, col)
+void printPixel(char row, char col, int color)
+{
+	int buff = 0x00000000;
+
+	buff = color;
+	buff = (buff << 8) | row;
+	buff = (buff << 8) | col;
+
+	MemoryWrite(OLED_BITMAP_RW, buff);
+}
+
+void sleep( unsigned int ms ) // fonction qui impose un delay en millisecondes
+{	// la fréquence d'horloge vaut 50 MHz
+	unsigned int t0 = MemoryRead( TIMER_ADR  );
+	while ( MemoryRead( TIMER_ADR  ) - t0 < 50000*ms ) // On compte 50000 périodes pour 1 ms
+		;
+}
--- C/projet_e2/Sources/main.c
+++ C/projet_e2/Sources/main.c
@@ -1,5 +1,8 @@
-#include "../../shared/plasmaSoPCDesign.h"
 #include "../../shared/plasma.h"
+#include "../../shared/plasmaCoprocessors.h"
+#include "../../shared/plasmaMyPrint.h"
+#include "../../shared/plasmaSoPCDesign.h"
+#include "../Includes/paint.h"
 
 #define MemoryRead(A)     (*(volatile unsigned int*)(A))
 #define MemoryWrite(A,V) *(volatile unsigned int*)(A)=(V)
@@ -7,6 +10,5 @@
 int main(int argc, char ** argv)
 {
 
-	puts("Hello world Projet E2 !\n");
 
 }
